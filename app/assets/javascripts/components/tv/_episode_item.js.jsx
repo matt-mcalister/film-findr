32d643804cb4e37a45e157c29922b4b990ac21e0
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-30 10:17:40 -0500

    set default image for tvitem

--- app/assets/javascripts/components/tv/_episode_item.js.jsx
+++ app/assets/javascripts/components/tv/_episode_item.js.jsx
@@ -1,8 +1,8 @@
 const EpisodeItem = ({ episode: { episodeName, filename, airedSeason, airedEpisodeNumber, in_plex } }) => {
-
+    const image_url = filename ? ("https://www.thetvdb.com/banners/" + filename) : "https://bigriverequipment.com/wp-content/uploads/2017/10/no-photo-available.png"
     return(
       <div className="item episode">
-        <img src={"https://www.thetvdb.com/banners/" + filename} alt={episodeName} />
+        <img src={image_url} alt={episodeName} />
         <h4>{airedEpisodeNumber}. {episodeName}</h4>
         {in_plex ? <h6>Currently In Plex</h6> : <button>Add To Plex</button>}
       </div>
