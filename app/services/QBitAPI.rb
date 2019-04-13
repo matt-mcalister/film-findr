0126ba10cfee86c98398940ce54699534dea79bb
matt-mcalister <matt.mcalister93@gmail.com>
2019-04-13 19:25:46 -0400

    created add_torrent method

--- app/services/QBitAPI.rb
+++ app/services/QBitAPI.rb
@@ -1,10 +1,23 @@
 class QBitAPI
-
+  BASE_URL = "http://localhost:8080/api/v2/torrents"
   # add torrents
     #  must always check first to see if torrent is already present
     #  adds to appropriate file path:
       # ~/Movies/Qbit/PlexPending/movies
       # ~/Movies/Qbit/PlexPending/tv-shows/#{NAME OF SHOW}/#{SEASON NUMBER}
+  def self.add_torrent(torrent_hash:, type:, magnet_url:, season: nil, episode: nil, show_title: nil)
+    case type
+    when :film
+      savepath = "/Users/MattMcAlister/Movies/Qbit/PlexPending/movies"
+    when :tv
+      savepath = "/Users/MattMcAlister/Movies/Qbit/PlexPending/tv-shows/#{show_title}/#{season}"
+    end
+
+    body = "hash=#{torrent_hash}&urls=#{magnet_url}&savepath=#{savepath}"
+
+    HTTParty.post(BASE_URL + "/add",{body: body})
+
+  end
 
   # check status of current torrents
     # if any torrent is done, move it to plex
@@ -12,8 +25,8 @@ class QBitAPI
       # once the season folder has been deleted, if the show folder is empty then delete the show folder
 
   def self.get_torrents
-    r = HTTParty.get("http://localhost:8080/api/v2/torrents/info")
+    r = HTTParty.get("#{BASE_URL}/info")
     r.parsed_response
   end
-  
+
 end
