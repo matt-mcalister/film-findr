7fec39d7210d6737005bd962260b9e243304a3d6
matt-mcalister <matt.mcalister93@gmail.com>
2019-04-28 17:21:51 -0400

    given and imdb_id renders json of best uhd_tor

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -20,6 +20,24 @@ class Api::V1::FilmsController < ApplicationController
     end
   end
 
+  def get_4k
+    rarbg = RARBG::API.new
+    torrent_results = rarbg.search(imdb: params[:imdb_id], category: [50, 51, 52],format: :json_extended)
+    if torrent_results.empty?
+      render json: {found_torrent: false}
+    else
+      categories = {"Movs/x265/4k/HDR" => 3, "Movies/x265/4k" => 2, "Movies/x264/4k" => 1 }
+      top_uhd_torrent = torrent_results.first
+      torrent_results[1..-1].each do |tor|
+        if categories[tor["category"]] > categories[top_uhd_torrent["category"]] || (categories[tor["category"]] == categories[top_uhd_torrent["category"]] && tor["seeders"] + tor["leechers"] > top_uhd_torrent["seeders"] + top_uhd_torrent["leechers"])
+          top_uhd_torrent = tor
+        end
+      end
+      render json: {found_torrent: true, torrent: top_uhd_torrent}
+
+    end
+  end
+
   def tv_search
 
     term = params[:search_term]
