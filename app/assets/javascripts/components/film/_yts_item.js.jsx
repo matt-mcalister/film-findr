dda91ee8ece2306c5adf75ba0d18cfed9bfd31a5
matt-mcalister <matt.mcalister93@gmail.com>
2019-04-28 17:51:26 -0400

    turned ytsitem into class component

--- app/assets/javascripts/components/film/_yts_item.js.jsx
+++ app/assets/javascripts/components/film/_yts_item.js.jsx
@@ -1,5 +1,5 @@
-const YTSItem = (props) => {
-  const download = (tor) => {
+class YTSItem extends React.Component {
+  download(tor){
     fetch("/api/v1/films/download", {
       method: "POST",
       headers: {
@@ -8,18 +8,21 @@ const YTSItem = (props) => {
       body: JSON.stringify({
         torrent_hash: tor.hash,
         type: "film",
-        magnet_url: `magnet:?xt=urn:btih:${tor.hash}&dn=${props.slug}&tr=udp://tracker.openbittorrent.com:80`
+        magnet_url: `magnet:?xt=urn:btih:${tor.hash}&dn=${this.props.slug}&tr=udp://tracker.openbittorrent.com:80`
       })
     })
   }
+
+  render() {
     return(
       <div className="item yts">
-        <img src={props.medium_cover_image} alt={props.title} />
-        <h4>{props.title} ({props.year})</h4>
-        <p>Download:</p>
-        {props.torrents.map(tor => {
-          return <button key={tor.hash} onClick={e => download(tor)}>{tor.type} - {tor.quality}</button>
-        })}
+      <img src={this.props.medium_cover_image} alt={this.props.title} />
+      <h4>{this.props.title} ({this.props.year})</h4>
+      <p>Download:</p>
+      {this.props.torrents.map(tor => {
+        return <button key={tor.hash} onClick={e => this.download(tor)}>{tor.type} - {tor.quality}</button>
+      })}
       </div>
     )
+  }
 }
