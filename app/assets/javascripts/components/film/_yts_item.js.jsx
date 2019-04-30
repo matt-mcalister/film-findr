70817ff0ea2481e82b266855d58e3e037a17ad48
matt-mcalister <matt.mcalister93@gmail.com>
2019-04-30 16:13:31 -0400

    added button to search for 4k torrent

--- app/assets/javascripts/components/film/_yts_item.js.jsx
+++ app/assets/javascripts/components/film/_yts_item.js.jsx
@@ -2,8 +2,32 @@ class YTSItem extends React.Component {
   constructor(props){
       super(props)
       this.state = {
-        torrents: props.torrents
+        uhd_searched: false,
+        uhd_tor: null,
+        searching: false,
       }
+      this.findUHD = this.findUHD.bind(this)
+  }
+
+  findUHD(){
+    this.setState({
+      searching: true,
+      uhd_searched: true,
+    })
+    fetch("/api/v1/films/4k", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        imdb_id: this.props.imdb_code
+      })
+    }).then(r => r.json()).then(json => {
+        this.setState({
+          searching: false,
+          uhd_tor: json.torrent
+        })
+    })
   }
 
   download(tor){
@@ -21,14 +45,23 @@ class YTSItem extends React.Component {
   }
 
   render() {
+    if (this.state.uhd_tor){
+      console.log(this.state.uhd_tor);
+    }
     return(
       <div className="item yts">
       <img src={this.props.medium_cover_image} alt={this.props.title} />
       <h4>{this.props.title} ({this.props.year})</h4>
       <p>Download:</p>
-      {this.state.torrents.map(tor => {
+      {this.props.torrents.map(tor => {
         return <button key={tor.hash} onClick={e => this.download(tor)}>{tor.type} - {tor.quality}</button>
       })}
+      {!this.state.uhd_searched ?
+        <button onClick={this.findUHD}>Check for 4K</button> :
+        this.state.searching ? <p>Searching...</p> :
+        !this.state.uhd_tor ? <p>No 4k Torrents Found</p> :
+        <button>4k Torrent</button>
+      }
       </div>
     )
   }
