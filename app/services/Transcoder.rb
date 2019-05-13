c1862cf941d10bd9f1feb61a9f6f9f33d43b2a4a
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-13 12:36:48 -0500

    wrote method to transcode all from folder, uses threadpool

--- app/services/Transcoder.rb
+++ app/services/Transcoder.rb
@@ -24,4 +24,14 @@ class Transcoder
     new_file_path = destination_path + file_name.gsub(from_extension,to_extension)
     self.class.handbrake.input(file_path).preset(preset).output(new_file_path)
   end
+
+  def self.transcode_from_folder(origin_folder:, destination_folder:, from_extension: "mkv", to_extension: "m4v", preset: "Apple 1080p30 Surround")
+    thread_pool = ThreadPool.new(5)
+    self.files_from_folder(origin_folder + "*.#{from_extension}", destination_folder).each do |transcoder|
+      thread_pool.schedule do
+        transcoder.transcode(from_extension: from_extension, to_extension: to_extension, preset: preset)
+        puts "Job #{transcoder.file_name}, finished by thread #{Thread.current[:id]}"
+      end
+    end
+  end
 end
