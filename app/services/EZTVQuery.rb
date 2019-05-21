19e2cac5b5afd4be44a7869ae0c2f0a619511b1a
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-21 14:43:30 -0400

    removed comments

--- app/services/EZTVQuery.rb
+++ app/services/EZTVQuery.rb
@@ -33,25 +33,6 @@ class EZTVQuery
       end
     end
     self.episodes
-    # each episode:
-    # {
-    #   "id"=>819872,
-    #   "hash"=>"fce1ccaa85bdfcbd25047f79a1037e8340d45ce5",
-    #   "filename"=>"Atlanta.S02E11.Crabs.in.a.Barrel.720p.AMZN.WEB-DL.DDP5.1.H.264-NTb[eztv].mkv",
-    #   "episode_url"=>"https://eztv.io/ep/819872/atlanta-s02e11-crabs-in-a-barrel-720p-amzn-web-dl-ddp5-1-h-264-ntb/",
-    #   "torrent_url"=>"https://zoink.ch/torrent/Atlanta.S02E11.Crabs.in.a.Barrel.720p.AMZN.WEB-DL.DDP5.1.H.264-NTb[eztv].mkv.torrent",
-    #   "magnet_url"=>"magnet:?xt=urn:btih:fce1ccaa85bdfcbd25047f79a1037e8340d45ce5&dn=Atlanta.S02E11.Crabs.in.a.Barrel.720p.AMZN.WEB-DL.DDP5.1.H.264-NTb%5Beztv%5D&tr=udp://tracker.coppersurfer.tk:80&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://exodus.desync.com:6969",
-    #   "title"=>"Atlanta S02E11 Crabs in a Barrel 720p AMZN WEB-DL DDP5 1 H 264-NTb EZTV",
-    #   "imdb_id"=>"4288182",
-    #   "season"=>"0",
-    #   "episode"=>"0",
-    #   "small_screenshot"=>"//ezimg.ch/thumbs/atlanta-s02e11-crabs-in-a-barrel-720p-amzn-web-dl-ddp5-1-h-264-ntb-small.jpg",
-    #   "large_screenshot"=>"//ezimg.ch/thumbs/atlanta-s02e11-crabs-in-a-barrel-720p-amzn-web-dl-ddp5-1-h-264-ntb-large.jpg",
-    #   "seeds"=>9,
-    #   "peers"=>6,
-    #   "date_released_unix"=>1526103345,
-    #   "size_bytes"=>"1188769570"
-    # }
   end
 
   def filter_episodes
