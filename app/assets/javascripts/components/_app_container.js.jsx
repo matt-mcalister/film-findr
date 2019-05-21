0075fa9fb54cb74d3aa54482ba76d186bd56adf2
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-21 15:00:54 -0400

    user can toggle between tv and film

--- app/assets/javascripts/components/_app_container.js.jsx
+++ app/assets/javascripts/components/_app_container.js.jsx
@@ -1,6 +1,33 @@
 class AppContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      mediaType: "film"
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    console.log(e.target.value);
+    console.log(e.target.selectedValue);
+    this.setState({
+      mediaType: e.target.value
+    })
+  }
 
   render(){
-    return null
+    return (
+      <div>
+        <select id="media-selector" name={"mediaType"} value={this.state.mediaType} onChange={this.handleChange}>
+          <option value={"film"}>Film Findr</option>
+          <option value={"tv"}>TV Findr</option>
+        </select>
+        {this.state.mediaType === "film" ?
+        <FilmMain />
+        :
+        <TVMain />
+        }
+      </div>
+    )
   }
 }
