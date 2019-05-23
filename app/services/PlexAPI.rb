9f7a749791d91f03f36307af45e886ce4c4c13f8
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-23 17:38:32 -0400

    now using rarbg for tv

--- app/services/PlexAPI.rb
+++ app/services/PlexAPI.rb
@@ -70,11 +70,14 @@ module PlexAPI
   def self.get_seasons_with_torrents(tvdb_id:, plex_id:)
     plex_seasons = TVDBQuery.get_seasons(tvdb_id: tvdb_id, plex_id: plex_id)
     imdbId = TVDBQuery.get_imdb_id(tvdb_id)
-    torrent_seasons = EZTVQuery.get_torrents_by_id(imdbId)
+    torrent_seasons = Rarbg::Search.get_torrents_by_id(imdbId)
     torrent_seasons.keys.each do |season| # season is a string
       torrent_seasons[season].keys.each do |episode| # episode is a string
         if !plex_seasons[season.to_i].nil? && !plex_seasons[season.to_i][episode.to_i].nil?
           plex_seasons[season.to_i][episode.to_i]["torrent_info"] = torrent_seasons[season][episode]
+        elsif episode == "full season"
+          plex_seasons[season.to_i]["full_season"] = {}
+          plex_seasons[season.to_i]["full_season"]["torrent_info"] = torrent_seasons[season][episode]
         end
       end
     end
