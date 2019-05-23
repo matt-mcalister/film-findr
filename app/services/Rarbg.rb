e4798caa4c823bec6fe9fe3c720c6c7df4211702
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-23 17:00:28 -0400

    added formatted episodes with extended json_payload

--- app/services/Rarbg.rb
+++ app/services/Rarbg.rb
@@ -26,19 +26,48 @@ module Rarbg
       sleep(1)
       rarbg = RARBG::API.new
       season_episode_string = "#{season && 'S' + (season.to_s.length < 10 ? '0' + season.to_s : season.to_s)}#{episode && 'E' + (episode.to_s.length < 10 ? '0' + episode.to_s : episode.to_s)}"
-      self.episodes = rarbg.search(imdb: "tt#{self.imdb_id}", category: [41], limit: 100, string: season_episode_string).map {|el| Rarbg::Episode.new(el)}
+      self.episodes = rarbg.search(imdb: "tt#{self.imdb_id}", category: [41], limit: 100, format: :json_extended, string: season_episode_string).map {|el| Rarbg::Download.new(el)}
+    end
+
+    def filter_episodes
+      self.episodes.each do |download|
+        if download.season.nil?
+          season = "0"
+          episode = "0"
+        else
+          season = download.season
+          if download.episode.nil?
+            episode = "full season"
+          else
+            episode = download.episode
+          end
+        end
+
+          self.formatted_episodes[season] ||= {}
+          self.formatted_episodes[season][episode] ||= []
+          self.formatted_episodes[season][episode] << download
+      end
+
+      self.formatted_episodes.keys.each do |season|
+        self.formatted_episodes[season].keys.each do |episode|
+          self.formatted_episodes[season][episode] = self.formatted_episodes[season][episode].max_by {|ep| ep.seeders}
+        end
+      end
+      self.formatted_episodes
     end
 
   end
 
-  class Episode
-    attr_reader :filename, :category, :magnet_url, :season, :episode
+  class Download
+    attr_reader :title, :magnet_url, :season, :episode, :seeders, :leechers
 
     def initialize(tor_hash)
-      @filename = tor_hash["filename"]
-      @category = tor_hash["category"]
+      @title = tor_hash["title"]
+      @seeders = tor_hash["seeders"]
+      @leechers = tor_hash["leechers"]
       @magnet_url = tor_hash["download"]
-      @season, @episode = tor_hash["filename"].scan(/S(\d+)+|E(\d+)+/).map {|arr| arr.compact.first.to_i.to_s}
+      @season = tor_hash["episode_info"]["seasonnum"]
+      @episode = tor_hash["episode_info"]["epnum"] if tor_hash["episode_info"]["epnum"] != "1000000"
     end
 
   end
