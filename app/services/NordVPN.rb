216dff31a397dc4abd08eac308fb4620ffcc6c53
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-28 11:11:04 -0400

    method to detect if nordvpn is open

--- app/services/NordVPN.rb
+++ app/services/NordVPN.rb
@@ -18,4 +18,17 @@ class NordVPN
     Dir["/Users/MattMcAlister/Library/Caches/com.nordvpn.osx/Logs/*.log"].max
   end
 
+  def self.active?
+    f = File.open(NordVPN.most_recent_log_file)
+    f.reverse_each do |log|
+      if log.include?("Disconnected")
+        return false
+      elsif log.include?("Connected to")
+        return true
+      else
+        nil
+      end
+    end
+  end
+  
 end
