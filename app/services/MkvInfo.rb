1b56718246e533110989da4f9927a4461557d86e
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-12 12:07:46 -0400

    class can detect if uhd

---
+++ app/services/MkvInfo.rb
@@ -0,0 +1,11 @@
+class MkvInfo
+  attr_reader :path, :output_string, :formatted_output, :uhd
+
+  def initialize(path)
+    raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exists?(path)
+    @path = path
+    @output_string = `mkvinfo #{path}`
+    @uhd = @output_string.match(/HEVC/)
+  end
+
+end
