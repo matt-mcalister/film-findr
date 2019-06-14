5c8a9ce976202b95c028f5604f47de2ffb768166
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-14 15:12:56 -0400

    removed 1080p only

--- app/services/YtsAPI.rb
+++ app/services/YtsAPI.rb
@@ -4,7 +4,7 @@ class YtsAPI
     if !NordVPN.active?
       NordVPN.restart
     end
-    response = HTTParty.get("https://yts.am/api/v2/list_movies.json?quality=1080p&limit=50&query_term=#{term}")
+    response = HTTParty.get("https://yts.am/api/v2/list_movies.json?limit=50&query_term=#{term}")
     if response && response["data"]["movies"]
       response["data"]["movies"].uniq
     else
