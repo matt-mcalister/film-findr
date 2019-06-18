53e36367f6f30b188eeb7311089d65826e817b00
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 10:33:55 -0400

    implemented loading component and keeps track of searchtype for results component

--- app/assets/javascripts/components/_landing.js.jsx
+++ app/assets/javascripts/components/_landing.js.jsx
@@ -3,23 +3,39 @@ class Landing extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      results: null
+      loading: false,
+      results: null,
+      searchType: null
     }
     this.handleSearch = this.handleSearch.bind(this)
+    this.beginSearch = this.beginSearch.bind(this)
   }
 
-  handleSearch(results){
+  handleSearch({ results, searchType }){
     this.setState({
-      results: results
+      loading: false,
+      results,
+      searchType
+    })
+  }
+
+  beginSearch(){
+    this.setState({
+      loading: true,
+      results: null,
+      searchType: null
     })
   }
 
   render() {
     return (
       <div>
-        <NavBar handleSearch={this.handleSearch}/>
+        <NavBar handleSearch={this.handleSearch} beginSearch={this.beginSearch}/>
         {
-          this.state.results && <SearchResults results={this.state.results} />
+          this.state.results && !this.state.loading && <SearchResults results={this.state.results} searchType={this.state.searchType} />
+        }
+        {
+          this.state.loading && <Loading />
         }
       </div>
     )
