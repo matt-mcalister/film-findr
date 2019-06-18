c8d9b3bbdcf0928982c01f5ab29994ccf97ab13b
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 10:35:51 -0400

    searchresults component now renders result items

--- app/assets/javascripts/components/_search_results.js.jsx
+++ app/assets/javascripts/components/_search_results.js.jsx
@@ -1,6 +1,12 @@
-const SearchResults = ({ results }) => {
+const SearchResults = ({ results, searchType }) => {
   console.log("RESULTS: ", results)
+  if (!results || results.length === 0) {
+    return <h1>Not Found</h1>
+  }
+  let resultItems = searchType === "film" ? results.map(item => <OMDBItem key={item.imdbID} {...item} />) : results.map(item => <TVDBItem key={item.id} {...item} />)
   return (
-    <h1>yo</h1>
+    <div className="flex flex-wrap search-results">
+      {resultItems}
+    </div>
   )
 }
