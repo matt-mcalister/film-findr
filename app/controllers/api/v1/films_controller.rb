a50a230f4e77de3b49a3dac4c3464bf5143708c6
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 21:27:21 -0400

    info method for /api/v1/films/info endpoint

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -15,6 +15,11 @@ class Api::V1::FilmsController < ApplicationController
     render json: torrents
   end
 
+  def info
+    omdb_info = OMDBQuery.find_by_imdb_id(params[:imdb_id])
+    render json: omdb_info
+  end
+
   def get_4k
     rarbg = RARBG::API.new
     torrent_results = rarbg.search(imdb: params[:imdb_id], category: [50, 51, 52],format: :json_extended)
