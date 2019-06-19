c1a84844f5a821d0742be4784011fa597861dc47
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 21:01:32 -0400

    search method handles qualities that are not 720 or 1080

--- app/services/YtsAPI.rb
+++ app/services/YtsAPI.rb
@@ -14,16 +14,16 @@ class YtsAPI
 
   def self.best_tor_by_imdb_id(imdb_id)
     tor_results = self.search(imdb_id).first
+    if tor_results.nil?
+      return {"720p" => nil, "1080p" => nil}
+    end
     formatted_results = {
       "720p" => [],
       "1080p" => []
     }
-    if tor_results.nil?
-      return formatted_results
-    end
     tor_results["torrents"].each_with_object(formatted_results) do |tor, hash|
       tor["source"] = "YTS"
-      hash[tor["quality"]] << tor
+      hash[tor["quality"]] << tor if hash[tor["quality"]]
     end
     formatted_results.keys.each do |key|
       formatted_results[key] = formatted_results[key].max_by {|tor| tor["seeds"]}
