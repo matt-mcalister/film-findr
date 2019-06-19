3e0723bc9fff7fbccbd12eeef3612f91910b8097
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 22:46:56 -0400

    methods to find tors by imdbID or tvdbID

--- app/services/QBitAPI.rb
+++ app/services/QBitAPI.rb
@@ -72,6 +72,14 @@ module QBitAPI
     r.parsed_response
   end
 
+  def self.find_by_imbd_id(imdbID)
+    QBitAPI::Torrent.all.find {|tor| tor.category["imdbID"] == imdbID}
+  end
+
+  def self.find_by_tvbd_id(tvdbID)
+    QBitAPI::Torrent.all.find {|tor| tor.category["tvdbID"] == tvdbID}
+  end
+
   class Torrent
 
       attr_accessor(:added_on,
