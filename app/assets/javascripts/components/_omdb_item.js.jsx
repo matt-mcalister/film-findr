4c07ef40372fc40c8bf7cc79b3216539800ed80e
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 21:04:21 -0400

    fixed default image poster

--- app/assets/javascripts/components/_omdb_item.js.jsx
+++ app/assets/javascripts/components/_omdb_item.js.jsx
@@ -4,7 +4,7 @@ const OMDBItem = (props) => {
       delete item.selectItem
       props.selectItem(item)
     }
-    let img = props.image_url === "N/A" ? "http://www.reelviews.net/resources/img/default_poster.jpg" : props.Poster
+    let img = props.Poster === "N/A" ? "http://www.reelviews.net/resources/img/default_poster.jpg" : props.Poster
     return(
       <div className="item center-text">
         <img src={img} alt={props.Title} />
