2d1697e45cd742da197489c0a9c4342843521d89
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 21:01:09 -0400

    fixed restart and active? to work with edge case where log file does not display status

--- app/services/NordVPN.rb
+++ app/services/NordVPN.rb
@@ -18,9 +18,17 @@ class NordVPN
     Dir["/Users/MattMcAlister/Library/Caches/com.nordvpn.osx/Logs/*.log"].max
   end
 
-  def self.active?
+  def self.nth_most_recent_log_file(n)
+    Dir["/Users/MattMcAlister/Library/Caches/com.nordvpn.osx/Logs/*.log"].sort[n * -1]
+  end
+
+  def self.active?(nth_attempt = nil)
     self.pid # this line ensures that nordvpn is open
-    f = File.open(NordVPN.most_recent_log_file)
+    if nth_attempt
+      f = File.open(NordVPN.nth_most_recent_log_file(nth_attempt))
+    else
+      f = File.open(NordVPN.most_recent_log_file)
+    end
     f.reverse_each do |log|
       if log.include?("Disconnected")
         return false
@@ -30,10 +38,19 @@ class NordVPN
         nil
       end
     end
+    nth_attempt ||= 1
+    if nth_attempt < 5
+      nth_attempt += 1
+      self.active?(nth_attempt)
+    else
+      return false
+    end
   end
 
   def self.restart
     `kill #{NordVPN.pid}`
+    sleep 0.5
     self.open
+    sleep 1
   end
 end
