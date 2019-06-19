7545d48175bb36ce713a6c9908c4f41e12b51737
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-18 22:44:27 -0400

    user can download film

--- app/assets/javascripts/components/_download_film.js.jsx
+++ app/assets/javascripts/components/_download_film.js.jsx
@@ -1,7 +1,7 @@
-const DownloadFilm = ({ tor, resolution }) => {
+const DownloadFilm = ({ tor, resolution, download }) => {
   let size = "size"
   if (tor.source === "YTS"){
     size = "size_bytes"
   }
-  return <button>{resolution} - {(tor[size] * 10**-9).toFixed(2)}GB</button>
+  return <button onClick={download}>{resolution} - {(tor[size] * 10**-9).toFixed(2)}GB</button>
 }
