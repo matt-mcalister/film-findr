3d1b9f13887436f6e55441a94d08a4b1c836bee9
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-02 14:46:20 -0400

    removed database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ config.webpacker.check_yarn_integrity = true
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
