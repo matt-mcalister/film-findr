92232e1c53cc3d40a82ea776ffb79c00f17e933e
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-24 09:43:48 -0400

    setup MkvInfo class to detect resolution in attribute 'size'

--- app/services/MkvInfo.rb
+++ app/services/MkvInfo.rb
@@ -1,10 +1,11 @@
 class MkvInfo
-  attr_reader :path, :output_string, :formatted_output, :uhd
+  attr_reader :path, :output_string, :uhd, :size
 
   def initialize(path)
     raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exists?(path)
     @path = path
-    @output_string = `mkvinfo #{path}`
+    @output_string = `mkvinfo "#{path}"`
+    @size = @output_string.split("Pixel height: ")[1].split("\n").first.to_i
     @uhd = !!@output_string.match(/HEVC/)
   end
 
